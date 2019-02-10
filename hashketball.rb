ae31753a143c77f08e57a217d3a334fcaf98ed45
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-10 23:33:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,58 @@ def game_hash
     :home => {
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
-      :players =>
+      :players => {
+        "Alan Anderson" => {
+          :number => 0,
+          :shoe => 16,
+          :points => 22 ,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 1
+        },
+        "Reggie Evans" => {
+          :number,
+          :shoe,
+          :points,
+          :rebounds,
+          :assists,
+          :steals,
+          :blocks,
+          :slam_dunks
+        },
+        "Brook Lopez" => {
+          :number,
+          :shoe,
+          :points,
+          :rebounds,
+          :assists,
+          :steals,
+          :blocks,
+          :slam_dunks
+        },
+        "Mason Plumlee" => {
+          :number,
+          :shoe,
+          :points,
+          :rebounds,
+          :assists,
+          :steals,
+          :blocks,
+          :slam_dunks
+        },
+        "Jason Terry" => {
+          :number,
+          :shoe,
+          :points,
+          :rebounds,
+          :assists,
+          :steals,
+          :blocks,
+          :slam_dunks
+        }
+      }
     }
     :away => {
       :team_name =>,
