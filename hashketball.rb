469a405237048760cdf18e85fbadf9cbd9a03160
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-10 23:29:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,16 @@
 # Write your code here!
+def game_hash
+  g_hash = {
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players =>
+    }
+    :away => {
+      :team_name =>,
+      :colors =>,
+      :players =>
+    }
+  }
 
-
-
-
-
-
-
-
-
-
+end
