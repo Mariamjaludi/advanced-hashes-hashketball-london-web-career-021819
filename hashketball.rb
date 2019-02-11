e9292670d4fec78d18ff648b1010ac4b08ef96e5
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-11 00:25:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -186,7 +186,7 @@ def player_numbers(name)
     #if team_data[:team_name] == name
       team_data[:players].each do |name, attributes|
     binding.pry    
-      end
-    end
+      
+    
   end
 end
